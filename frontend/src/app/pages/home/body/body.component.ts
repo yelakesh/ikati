dcfc281199fb0d9d3f51111afdc51954ab9bf915
yelakesh <davidsampalo@gmail.com>
2025-06-10 23:23:57 +0200

    Algún banner de ofertas y cupón y cambio de imagen en el selector-animal

--- frontend/src/app/pages/home/body/body.component.ts
+++ frontend/src/app/pages/home/body/body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GridProductosComponent } from "./grid-productos/grid-productos.component";
 import { SelectorAnimalComponent } from "../../../components/selector-animal/selector-animal.component";
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-body',
-  imports: [GridProductosComponent, SelectorAnimalComponent],
+  imports: [GridProductosComponent, SelectorAnimalComponent,RouterLink],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
