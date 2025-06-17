2e9475d606a45c2dfb325563c1554314cfb31b3a
Lunnaa0 <lajm5741@gmail.com>
2025-06-17 03:47:57 +0200

    cors

--- backend/index.js
+++ backend/index.js
@@ -10,15 +10,7 @@ const allowedOrigins = [
   'http://localhost:4200',
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('No permitido por CORS'));
-    }
-  }
-}));
+app.use(cors());
 
 app.use(express.json()); 
 
