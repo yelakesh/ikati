726155393a4da519077b4dd1bf69f3ce2561ad10
Lunnaa0 <lajm5741@gmail.com>
2025-06-17 03:27:07 +0200

    logs

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,10 @@ const { Pool } = require('pg');
 
 let pool;
 
+console.log("DB_ENGINE:", process.env.DB_ENGINE);
+
 if (process.env.DB_ENGINE === 'mysql') {
+    console.log("Usando MySQL");
   pool = mysql.createPool({
     host: process.env.MYSQLHOST || "localhost",
     user: process.env.MYSQLUSER || "root",
@@ -15,6 +18,7 @@ if (process.env.DB_ENGINE === 'mysql') {
     queueLimit: 0
   });
 } else if (process.env.DB_ENGINE === 'postgresql') {
+   console.log("Usando PostgreSQL");
   pool = new Pool({
     host: process.env.PGHOST,
     user: process.env.PGUSER,
@@ -22,6 +26,12 @@ if (process.env.DB_ENGINE === 'mysql') {
     database: process.env.PGDATABASE,
     port: process.env.PGPORT,
   });
+ pool.connect()
+    .then(client => {
+      console.log("Conexión a PostgreSQL exitosa");
+      client.release();
+    })
+    .catch(err => console.error("Error al conectar con PostgreSQL:", err));
 } else {
   throw new Error('DB_ENGINE no especificada o no soportada.');
 }
